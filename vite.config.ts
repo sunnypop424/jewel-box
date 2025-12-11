74b31d67f85d52f6ebebd5323570c2bb0af23dda
sunnypop424 <sunnypop424@gmail.com>
2025-12-11 16:50:05 +0900

    수정내용 반영

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// repo 이름에 맞게 '/jewel-box/' 수정
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/jewel-box/',
+});
